95a26ecbda9bf3ff22f9c3cf39f80c35a9c15b7f
Michelle-715 <michellesagredo1@gmail.com>
2025-06-18 16:33:13 +1000

    Add Katsu Curry Corner program
    * Initialise Katsu Curry Corner program
    
    * feat: add curry size selection
    
    * feat: add extra ingredients
    
    * feat: add meal upgrade with side and drink
    
    * feat: add goodbye message

---
+++ katsu.py
@@ -0,0 +1,17 @@
+print("Welcome to Katsu Curry Corner!")
+
+size = input("What size of Katsu Curry would you like? (Small/Medium/Large): ")
+print(f"You chose a {size} Katsu Curry.")
+
+extras = input("Any extra ingredients?: ")
+print(f"Adding the following extras: {extras}")
+
+meal = input("Would you like to make it a meal? (yes/no): ")
+if meal.lower() == "yes":
+    side = input("Choose a side (Edamame, Vegetable Tempura, Miso Tofu): ")
+    drink = input("Choose a drink (Soda, Juice, Water, Tea): ")
+    print(f"Meal selected with {side} and {drink}.")
+else:
+    print("No meal selected.")
+
+print("Enjoy your custom katsu curry from Katsu Curry Corner! Come again soon!")
